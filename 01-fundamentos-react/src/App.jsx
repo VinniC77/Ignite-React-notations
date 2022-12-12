c7d7223a7e48ad098d73b3fc5afa5544404a77cf
viniciusClunc <vinicius@valorizza.com.br>
2022-12-12 18:23:13 -0300

    Sidebar

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -1,26 +1,32 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 
-import './global.css'
+import styles from "./App.module.css";
+
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
-
       <Header />
-
-      <Post
-        author="Vinicius Clunc"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus alias molestiae consequatur provident ipsam nostrum, modi animi!"
-      />
-      <Post
-        author="Jordan Peterson"
-        content="Lorem ipsum dolor, adipisicing elit. Necessitatibus alias molestiae sit amet consectetur adipisicing elit. Necessitatibus alias molestiae consequatur provident ipsam nostrum, modi animi!"
-      />
-      <Post
-        author="Margaret Tatcher"
-        content="Lorem ipsum dolor, adipisicing elit. Necessitatibus alias molestiae sit amet consectetur adipisicing dolor, adipisicing elit. Necessitatibus alias molestiae sit amet elit. Necessitatibus alias molestiae consequatur provident ipsam nostrum, modi animi!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Vinicius Clunc"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus alias molestiae consequatur provident ipsam nostrum, modi animi!"
+          />
+          <Post
+            author="Jordan Peterson"
+            content="Lorem ipsum dolor, adipisicing elit. Necessitatibus alias molestiae sit amet consectetur adipisicing elit. Necessitatibus alias molestiae consequatur provident ipsam nostrum, modi animi!"
+          />
+          <Post
+            author="Margaret Tatcher"
+            content="Lorem ipsum dolor, adipisicing elit. Necessitatibus alias molestiae sit amet consectetur adipisicing dolor, adipisicing elit. Necessitatibus alias molestiae sit amet elit. Necessitatibus alias molestiae consequatur provident ipsam nostrum, modi animi!"
+          />
+        </main>
+      </div>
     </div>
   );
 }
