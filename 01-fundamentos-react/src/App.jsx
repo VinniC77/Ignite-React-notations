e0f9158adf934b3703b6dcf02f0a4dfdc1974a2c
viniciusClunc <vinicius@valorizza.com.br>
2022-12-12 15:05:08 -0300

    header

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -1,8 +1,14 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+import './global.css'
 
 export function App() {
   return (
     <div>
+
+      <Header />
+
       <Post
         author="Vinicius Clunc"
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus alias molestiae consequatur provident ipsam nostrum, modi animi!"
