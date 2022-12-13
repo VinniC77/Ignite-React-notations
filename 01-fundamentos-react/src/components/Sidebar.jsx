54ef1b461a367818a4dd601ab27bb1671e0ce0d7
viniciusClunc <vinicius@valorizza.com.br>
2022-12-13 11:48:10 -0300

    HTML e CSS finalizados

--- 01-fundamentos-react/src/components/Sidebar.jsx
+++ 01-fundamentos-react/src/components/Sidebar.jsx
@@ -1,17 +1,23 @@
+import { Pen, PencilLine } from "phosphor-react";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img className={styles.cover} src="https://images.unsplash.com/photo-1579403124614-197f69d8187b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1579403124614-197f69d8187b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
 
       <div className={styles.profile}>
-        <strong>Vinícius Clunc</strong>       
+        <img className={styles.avatar} src="https://github.com/VinniC77.png" />
+        <strong>Vinícius Clunc</strong>
         <span>Web Developer</span>
       </div>
       <footer>
         <a href="#">
-            Editar seu perfil
+          <PencilLine size={20} />
+          Editar seu perfil
         </a>
       </footer>
     </aside>
